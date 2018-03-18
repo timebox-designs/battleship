6ca846ffe333fa2090c4709a0b9225b12294bbf1
Timebox Designs <timebox-designs@gmail.com>
2018-03-17 19:24:31 -0600

    Cleaned up socket methods.

--- client/src/util/socket.js
+++ client/src/util/socket.js
@@ -3,46 +3,26 @@
 import socketIOClient from 'socket.io-client';
 import sailsIOClient from 'sails.io.js';
 
-let io = sailsIOClient(socketIOClient);
+const io = sailsIOClient(socketIOClient);
 io.sails.url = 'http://localhost:1337';
 
 const errorCodes = [400, 404, 500];
 const match = (first) => (second) => first === second;
 
+const request = (options) => {
+    return new Promise((resolve, reject) => {
+        io.socket.request(options, (data, jwr) => {
+            if (errorCodes.some(match(jwr.statusCode))) return reject(jwr.statusCode);
+            return resolve(data);
+        });
+    });
+};
+
 const socket = {
-    get(url, options = {}) {
-        return new Promise((resolve, reject) => {
-            io.socket.get(url, options, (data, jwr) => {
-                // console.log('get', data, jwr);
-                if (errorCodes.some(match(jwr.statusCode))) return reject(jwr.statusCode);
-                return resolve(data);
-            });
-        });
-    },
-
-    put(url, options = {}) {
-        return new Promise((resolve, reject) => {
-            io.socket.put(url, options, (data, jwr) => {
-                console.log('put', data, jwr);
-                if (errorCodes.some(match(jwr.statusCode))) return reject(jwr.statusCode);
-                return resolve(data);
-            });
-        });
-    },
-
-    post(url, options = {}) {
-        return new Promise((resolve, reject) => {
-            io.socket.post(url, options, (data, jwr) => {
-                // console.log('post', data, jwr);
-                if (errorCodes.some(match(jwr.statusCode))) return reject(jwr.statusCode);
-                return resolve(data);
-            });
-        });
-    },
-
-    on(event, msg) {
-        return io.socket.on(event, msg);
-    }
+    post: (url, data) => request({method: 'POST', url, data}),
+    put: (url, data) => request({method: 'PUT', url, data}),
+    get: (url, data) => request({method: 'GET', url, data}),
+    on: (event, msg) => io.socket.on(event, msg)
 };
 
 export default socket;
